7bf2222c86593c76cd541af21b6900b9d3b9e69f
SPNgcobo <ngcobosamkelop@gmail.com>
2025-03-14 10:20:42 +0200

    End of Section 8 (Extending the API)

--- API/Program.cs
+++ API/Program.cs
@@ -3,7 +3,9 @@
 // Project: Dating App
 
 using API;
+using API.Data;
 using API.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,5 +26,20 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
