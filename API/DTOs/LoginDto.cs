ab16e0605a206c99d067c3e075cbe0323bf92122
SPNgcobo <ngcobosamkelop@gmail.com>
2025-03-22 13:02:25 +0200

    End of Section 11 (Implemented photo upload functionality)

--- API/DTOs/LoginDto.cs
+++ API/DTOs/LoginDto.cs
@@ -2,6 +2,6 @@
 
 public class LoginDto
 {
-    public required string UserName { get; set; }
+    public required string Username { get; set; }
     public required string Password { get; set; }
 }
