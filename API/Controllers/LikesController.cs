e4a0e08ff167db5dd369f1e6aae3c6deb793ce22
SPNgcobo <ngcobosamkelop@gmail.com>
2025-05-02 19:25:18 +0200

    End of Section 18 (Implemented the Unit of Work pattern, added a confirm dialog service)

--- API/Controllers/LikesController.cs
+++ API/Controllers/LikesController.cs
@@ -2,12 +2,13 @@
 using API.DTOs;
 using API.Extensions;
 using API.Helpers;
+using API.Interfaces;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API;
 
-public class LikesController(ILikesRepository likesRepository) : BaseApiController
+public class LikesController(IUnitOfWork unitOfWork) : BaseApiController
 {
     [HttpPost("{targetUserId:int}")]
     public async Task<ActionResult> Togglelike(int targetUserId)
@@ -15,7 +16,7 @@ public class LikesController(ILikesRepository likesRepository) : BaseApiControll
         var sourceUserId = User.GetUserId();
         if (sourceUserId == targetUserId) return BadRequest("You cannon like yourself");
 
-        var existingLike = await likesRepository.GetUserLike(sourceUserId, targetUserId);
+        var existingLike = await unitOfWork.LikesRepository.GetUserLike(sourceUserId, targetUserId);
 
         if (existingLike == null)
         {
@@ -25,14 +26,14 @@ public class LikesController(ILikesRepository likesRepository) : BaseApiControll
                 TargetUserId = targetUserId
             };
 
-            likesRepository.AddLike(like);
+            unitOfWork.LikesRepository.AddLike(like);
         }
         else
         {
-            likesRepository.DeleteLike(existingLike);
+            unitOfWork.LikesRepository.DeleteLike(existingLike);
         }
 
-        if (await likesRepository.SaveChanges()) return Ok();
+        if (await unitOfWork.Complete()) return Ok();
 
         return BadRequest("Failed to update like");
     }
@@ -41,7 +42,7 @@ public class LikesController(ILikesRepository likesRepository) : BaseApiControll
 
     public async Task<ActionResult<IEnumerable<int>>> GetCurrentUserLikeIds()
     {
-        return Ok(await likesRepository.GetCurrentUserLikeIds(User.GetUserId()));
+        return Ok(await unitOfWork.LikesRepository.GetCurrentUserLikeIds(User.GetUserId()));
     }
 
     [HttpGet]
@@ -50,7 +51,7 @@ public class LikesController(ILikesRepository likesRepository) : BaseApiControll
     {
 
         likesParams.UserId = User.GetUserId();
-        var users = await likesRepository.GetUserLikes(likesParams);
+        var users = await unitOfWork.LikesRepository.GetUserLikes(likesParams);
 
         Response.AddPaginationHeader(users);
 
